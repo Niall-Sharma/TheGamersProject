f545f03ea2e3272b34927816f92be2b1c2063d74
Niall Sharma <Niall.k.sharma@gmail.com>
2023-08-31 13:29:33 -0500

    Start of prototype

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed= 20;
-    
+    public float speed = 20f;
+    public Rigidbody2D playerRB;
+    private Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxis("Horizontal");
+
+        playerRB.AddForce(movement*speed*Time.deltaTime);
     }
 }
