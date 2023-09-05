ae3cd88fcb9438b4d79d01f037ec9c811b5fac57
PerpetualObligation <jl46692@wdmcs.org>
2023-09-05 13:41:18 -0500

    Update Collectables.cs

--- Assets/Collectables.cs
+++ Assets/Collectables.cs
@@ -4,29 +4,8 @@ using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
-  //  public Object thisObject;
-    public BoxCollider2D bc; //the hitbox of the collectible
-
-// private void Awake()
-// {
-
-// } 
-
-void Start()
-    {
-//make them appear here
-    
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // collectibles wabble a bit?
-        // make them pickupable 
-            // if player touches it, it disappears and ??
-        if(collision.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-        }
-    }
+ // private Object thisObject;
+//  private void Awaken(){
+//    
+  //}
 }
