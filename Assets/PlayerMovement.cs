ba39fa49d1d18242e1d2310eb5e21ebbc9938658
raju7274 <rs44951@wdmcs.org>
2023-09-07 13:22:30 -0500

    Can only Jump once

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     Vector2 movement;
+    bool isGrounded = false;
+   
     public float jumpForce=250;
       // Start is called before the first frame update
 
+    
 
     void Start()
     {
@@ -20,8 +23,26 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal");
         rb.AddForce(movement*speed*Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space)&&isGrounded){
             rb.AddForce(Vector2.up * jumpForce);
         }
+        
+
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = false;
+        }
+
+    }
+
 }
