c7f5d75ab291f7bc18a55c10e6796cc846f53b29
Spork06 <spenserpraska@gmail.com>
2023-09-07 13:36:21 -0500

    Main menu adijwa

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -15,4 +15,20 @@ public class MainMenu : MonoBehaviour
     }
 
 
+    public void Level1(){
+        SceneManager.LoadScene(1);
+    }
+    public void Level2(){
+        SceneManager.LoadScene(2);
+    }
+    public void Level3(){
+        SceneManager.LoadScene(3);
+    }
+    public void Level4(){
+        SceneManager.LoadScene(4);
+    }
+    public void Level5(){
+        SceneManager.LoadScene(5);
+    }
+
 }
