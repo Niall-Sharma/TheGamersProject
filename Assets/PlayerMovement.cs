55a6c3c2cd528408420daa1a729abbaa7b396787
PerpetualObligation <jl46692@wdmcs.org>
2023-09-07 13:25:58 -0500

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     Vector2 movement;
+    bool isGrounded = false;
+   
     public float jumpForce=250;
       // Start is called before the first frame update
 
+    
 
     void Start()
     {
@@ -21,9 +24,29 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal");
         rb.AddForce(movement*speed*Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space)&&isGrounded){
             rb.AddForce(Vector2.up * jumpForce);
+        }
+        
+
+    }
+
+<<<<<<< HEAD
+=======
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            isGrounded = true;
         }
     }
 
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = false;
+        }
+
+    }
+
+>>>>>>> 5a63f88270587f713658736db30bf59a8af48c7e
 }
