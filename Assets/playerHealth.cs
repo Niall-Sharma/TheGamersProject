a4fc49cb81eb645735098d4f68567ba09709f41a
Niall Sharma <Niall.k.sharma@gmail.com>
2023-09-07 13:48:10 -0500

    added Heatlh

---
+++ Assets/playerHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+
+    public int health;
+
+
+    public void takeDamage(int damage){
+        health -= damage;
+        Debug.Log("PLAYER HEALTH: " + health);
+        if(health <= 0){
+            playerDie();
+        }
+    }
+
+    public void playerDie(){
+        Destroy(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
